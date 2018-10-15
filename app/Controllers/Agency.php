4080154375ea7444ba4a4b9a1eba13e3d9400689
tguyonnet <35605917+tguyonnet@users.noreply.github.com>
2018-10-15 20:35:19 +0200

    Update Agency.php

--- app/Controllers/Agency.php
+++ app/Controllers/Agency.php
@@ -31,20 +31,17 @@ class Agency
     }
 
     public function vehicleOccupiedByDate($request, $response, $args){
-        //debut < fin recherche et fin > debut recherche
-//        $vehicle = R::findAll('vehicle', ' agency_id = ? and model_id= ? ', [$args['agency_id'],$args['model_id']]);
-//        var_dump($vehicle); exit();
-//        $rental = R::findAll('rental', 'vehicle_id = ?  and start_date < ? and end_date > ?', [$vehicle['id'],$args['start_date'].$args['end_date']]);
-//        var_dump($rental); exit();
-        $vehiculesAgence = R::getAll('SELECT DISTINCT manufacturer.label as Marque,model.label as Modele,category.label as Categorie,number_plate as Plaque 
+        $vehicleAgency = R::getAll('SELECT DISTINCT manufacturer.label as Marque, model.label as Modele, category.label as Categorie, numberPlate as Plaque 
                   FROM model,manufacturer,agency,category,vehicle,rental 
+                  
                   WHERE model.manufacturer_id = manufacturer.id 
                   AND model.category_id = category.id 
                   AND vehicle.agency_id = agency.id 
                   AND vehicle.model_id = model.id 
-                  AND agency.id = ? 
+                  
+                  AND agency.id = :agency_id
                   AND rental.start_date < rental.end_date 
-                  AND rental.end_date > rental.start_date;',[$args['codeAgence']] );
-        return $response->withJson(['data' => $vehiculesAgence]);
+                  AND rental.end_date > rental.start_date;',[':agency_id'=>$args['agency_id']]);
+        return $response->withJson(['data' => $vehicleAgency]);
     }
 }
