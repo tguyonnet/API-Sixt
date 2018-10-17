bd7f0f5a96f42c94cb055f5cc16ea13e3d6af943
tguyonnet <35605917+tguyonnet@users.noreply.github.com>
2018-10-17 18:59:16 +0200

    Add files via upload

--- app/Controllers/Agency.php
+++ app/Controllers/Agency.php
@@ -14,22 +14,49 @@ use RedBeanPHP\R;
 
 class Agency
 {
-
+    /**
+     * liste de toutes les agences
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public static function agency($request, $response, $args){
         $agency = R::findAll('agency');
         return $response->withJson(['data'=>$agency]);
     }
 
+    /**
+     * Informations d'une agence (code, adresse, code postal, ville)
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function agency_id($request, $response, $args){
         $agency = R::findOne('agency','id= ? ', [$args['agency_id']]);
         return $response->withJson(['data'=>$agency]);
     }
 
+    /**
+     * Liste des véhicules d'une agence
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function vehicleByAgency($request, $response, $args){
         $vehicle = R::findAll('vehicle','agency_id= ? ', [$args['agency_id']]);
         return $response->withJson(['data'=>$vehicle]);
     }
 
+    /**
+     * Liste des véhicules disponibles d'une agence
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function vehicleOccupiedByDate($request, $response, $args){
         $vehicleAgency = R::getAll('SELECT DISTINCT manufacturer.label as Marque, model.label as Modele, category.label as Categorie, numberPlate as Plaque 
                   FROM model,manufacturer,agency,category,vehicle,rental 
--- app/Controllers/Rental.php
+++ app/Controllers/Rental.php
@@ -15,17 +15,37 @@ use RedBeanPHP\R;
 class Rental
 {
 
-
+    /**
+     * Liste des réservations du client
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function rentalByCustomer($request, $response, $args){
         $rental = R::findAll('rental', 'customer_id= ? ', [$args['customer_id']]);
         return $response->withJson(['data'=>$rental]);
     }
 
+    /**
+     * Liste des réservations du client depuis une date
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function rentalByCustomerByDate($request, $response, $args){
         $rental = R::getAll(' select * from rental where customer_id= :customer_id and start_date>= :start_date', [':customer_id'=>$args['customer_id'],':start_date'=>$args['start_date']]);
         return $response->withJson(['data'=>$rental]);
     }
 
+    /**
+     * Enregistrer une réservation
+     * @param $request
+     * @param $response
+     * @param $args
+     * @return mixed
+     */
     public function createRental($request, $response, $args){
         $rental = R::dispense('rental');
         $rental->vehicle_id = $args['vehicle_id'];
